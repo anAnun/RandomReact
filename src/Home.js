336bfc1e26fa804cd0771f3cd23ef5b5402bd4b1
Allan Yeznaian <allanyeznaian@gmail.com>
2018-11-18 17:54:54 -0800

    a

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./HomeHeader.css";
 import SocialMediaIcons from "./SocialMediaIcons";
 import Pdf from "./images/AllanYeznaian.pdf";
+import image1 from "./images/Slider1.jpg";
+import image2 from "./images/Slider2.jpg";
 import RecruitHub from "./images/recruithub.png";
 
 class Home extends React.Component {
@@ -64,22 +66,34 @@ class Home extends React.Component {
               related to programming.
             </div>
             <br />
-            <hr className="line-after-description" />
-            <div className="middle-text">Previous Jobs</div>
-            <hr className="line-after-description" />
-            <img className="Recruit-Hub-image" src={RecruitHub} />
-            <div className="text-margin">
-              Recruit Hub Sports is a professional online platform for high
-              school athletes to be discovered by recruiting coaches from
-              national colleges and universities. I helped build this app using
-              React 16 and ES6. The server was built using ASP.NET Web API 2 on
-              .NET Framework 4.6. WebSockets using SignalR were used to publish
-              real-time updates to browser clients. Third-party authentication
-              via Google was integrated usingOAuth 2. SQL Server 2017 was used
-              for data storage, including the new JSON reading and writing
-              functionality.
+            <div className="container-css margin">
+              <section className="slideshow">
+                <div className="slideshow-container slide">
+                  <div className="slider1-css">
+                    <img alt="" src={image1} className="auto-size" />
+                    <img alt="" src={image2} className="img1 auto-size" />
+                  </div>
+                </div>
+              </section>
+              <hr className="line-after-description" />
+              <div className="middle-text">Previous Jobs</div>
+              <hr className="line-after-description" />
+              <a className="content-1" href="https://www.recruithubsports.com/">
+                <img className="Recruit-Hub-image" src={RecruitHub} />
+              </a>
+              <div className="text-margin content-2">
+                Recruit Hub Sports is a professional online platform for high
+                school athletes to be discovered by recruiting coaches from
+                national colleges and universities. I helped build this app
+                using React 16 and ES6. The server was built using ASP.NET Web
+                API 2 on .NET Framework 4.6. WebSockets using SignalR were used
+                to publish real-time updates to browser clients. Third-party
+                authentication via Google was integrated usingOAuth 2. SQL
+                Server 2017 was used for data storage, including the new JSON
+                reading and writing functionality.
+              </div>
+              <br />
             </div>
-            <br />
           </div>
         </div>
       </React.Fragment>
